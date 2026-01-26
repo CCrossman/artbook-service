e40f9ab185b8dbf9cb10c015b448aa95a698802a
Chris Crossman <christopher.j.crossman@gmail.com>
2026-01-26 13:31:15 -0700

    Dockerfile renames jar so take advantage of that

--- artbook-service/pre_deploy.sh
+++ artbook-service/pre_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Starting Pre-Deploy Migrations..."
-java -cp target/artbook-service.jar com.artbook.service.job.PreDeployJob
+java -cp target/app.jar com.artbook.service.job.PreDeployJob
 
 # Exit with success status (0) or Render will abort the deploy
 exit 0
