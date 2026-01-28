309cd0ea02cbce5bac71a85431947581c2638d38
Chris Crossman <christopher.j.crossman@gmail.com>
2026-01-27 20:45:14 -0700

    configuration can fail soft or hard. add logging

--- artbook-service/src/main/java/com/artbook/service/controller/StatusController.java
+++ artbook-service/src/main/java/com/artbook/service/controller/StatusController.java
@@ -1,10 +1,13 @@
 package com.artbook.service.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.*;
@@ -12,12 +15,16 @@ import java.sql.*;
 @RestController
 @RequestMapping("/api/v1/status")
 public class StatusController {
+    private static final Logger logger = LoggerFactory.getLogger(StatusController.class);
 
     @Value("{app.location.config}")
     private String configPath;
 
     @Value("{app.location.storage}")
     private String storagePath;
+
+    @Value("{health.verify.fail-hard}")
+    private Boolean failHard;
 
     // GET http://localhost:8080/api/v1/status/health
     @GetMapping("/health")
@@ -33,25 +40,40 @@ public class StatusController {
         return "System is up and running!";
     }
 
-    private static void verifyDriverConnection() throws SQLException {
-        String postgresURL = System.getenv("DATABASE_URL");
+    private void verifyDriverConnection() throws SQLException {
+        try {
+            String postgresURL = System.getenv("DATABASE_URL");
+            logger.atDebug().log("database url: " + postgresURL);
 
-        try (Connection conn = DriverManager.getConnection(postgresURL)) {
-            try (PreparedStatement stmt = conn.prepareStatement("SELECT 1")) {
-                try (ResultSet rs = stmt.executeQuery()) {
-                    boolean hasRecord = rs.next();
-                    if (!hasRecord) {
-                        throw new SQLException("SELECT 1 returned nothing");
+            try (Connection conn = DriverManager.getConnection(postgresURL)) {
+                try (PreparedStatement stmt = conn.prepareStatement("SELECT 1")) {
+                    try (ResultSet rs = stmt.executeQuery()) {
+                        boolean hasRecord = rs.next();
+                        if (!hasRecord) {
+                            throw new SQLException("SELECT 1 returned nothing");
+                        }
                     }
                 }
+            }
+        } catch (SQLException sqlEx) {
+            logger.error("verify driver connection failed", sqlEx);
+            if (Boolean.TRUE.equals(failHard)) {
+                throw sqlEx;
             }
         }
     }
 
-    private static void verifyPathUsable(String rawPath) throws IOException {
-        Path path = Paths.get(rawPath);
-        if (!Files.isReadable(path) || !Files.isWritable(path)) {
-            throw new IOException("Cannot read or write '" + rawPath + "'");
+    private void verifyPathUsable(String rawPath) throws IOException {
+        try {
+            Path path = Paths.get(rawPath);
+            if (!Files.isReadable(path) || !Files.isWritable(path)) {
+                throw new IOException("Cannot read or write '" + rawPath + "'");
+            }
+        } catch (InvalidPathException | IOException ex) {
+            logger.error("verify path usable failed", ex);
+            if (Boolean.TRUE.equals(failHard)) {
+                throw ex;
+            }
         }
     }
 }
