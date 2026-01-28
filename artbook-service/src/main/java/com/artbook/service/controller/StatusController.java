e8d1f2233652e16271cd35971d42d47194c46984
Chris Crossman <christopher.j.crossman@gmail.com>
2026-01-27 19:56:23 -0700

    Integrate with DB and Persistent Disk

--- artbook-service/src/main/java/com/artbook/service/controller/StatusController.java
+++ artbook-service/src/main/java/com/artbook/service/controller/StatusController.java
@@ -1,14 +1,43 @@
 package com.artbook.service.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.*;
 
 @RestController
 @RequestMapping("/api/v1/status")
 public class StatusController {
 
+    @Value("{app.location.config}")
+    private String configPath;
+
     // GET http://localhost:8080/api/v1/status/health
     @GetMapping("/health")
-    public String checkHealth() {
+    public String checkHealth() throws SQLException, IOException {
+        String postgresURL = System.getenv("DATABASE_URL");
+
+        // connect to artbook-db
+        try (Connection conn = DriverManager.getConnection(postgresURL)) {
+            try (PreparedStatement stmt = conn.prepareStatement("SELECT 1")) {
+                try (ResultSet rs = stmt.executeQuery()) {
+                    boolean hasRecord = rs.next();
+                    if (!hasRecord) {
+                        throw new SQLException("SELECT 1 returned nothing");
+                    }
+                }
+            }
+        }
+
+        // connect to file space
+        if (!Files.exists(Paths.get(configPath))) {
+            throw new IOException("'" + configPath + "' does not exist");
+        }
+
+        // everything looks good
         return "System is up and running!";
     }
 }
