bc1b0b5d4ab73cf7c28faa9d3c6f56db0f04d593
Chris Crossman <christopher.j.crossman@gmail.com>
2026-01-30 18:53:59 -0700

    Read from .env

---
+++ artbook-service/src/main/java/com/artbook/service/config/EnvConfig.java
@@ -0,0 +1,13 @@
+package com.artbook.service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+    @PropertySource(value = "file:./.env", ignoreResourceNotFound = true)
+})
+public class EnvConfig {
+
+}
