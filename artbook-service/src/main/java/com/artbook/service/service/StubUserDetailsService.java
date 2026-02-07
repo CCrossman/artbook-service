6fee65af8a2c9976589ac1a7b0c234af88c2a0bf
Christopher Crossman <CCrossman@users.noreply.github.com>
2026-02-06 18:52:47 -0700

    Fix stubbed password (#13)

--- artbook-service/src/main/java/com/artbook/service/service/StubUserDetailsService.java
+++ artbook-service/src/main/java/com/artbook/service/service/StubUserDetailsService.java
@@ -1,22 +1,36 @@
 package com.artbook.service.service;
 
 import com.artbook.service.domain.StubUserDetails;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StubUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    // TODO
+    private String stubbedPassword;
+
+    @PostConstruct
+    public void init() {
+        this.stubbedPassword = passwordEncoder.encode("STUBBED");
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         if ("seejo.crux.x@gmail.com".equals(username)) {
-            return new StubUserDetails(username, "registered-artist");
+            return new StubUserDetails(username, stubbedPassword, "registered-artist");
         }
         if ("seejo.crux@gmail.com".equals(username)) {
-            return new StubUserDetails(username, "registered-viewer");
+            return new StubUserDetails(username, stubbedPassword, "registered-viewer");
         }
-        return new StubUserDetails(username, "guest");
+        return new StubUserDetails(username, null, "guest");
     }
 }
