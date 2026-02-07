62acb514269b5c8919ba471927ef15840f72ee9b
Christopher Crossman <CCrossman@users.noreply.github.com>
2026-02-06 19:00:31 -0700

    fix dependency cycle by making encrypted password an injected variable (#14)

--- artbook-service/src/main/java/com/artbook/service/service/StubUserDetailsService.java
+++ artbook-service/src/main/java/com/artbook/service/service/StubUserDetailsService.java
@@ -1,27 +1,17 @@
 package com.artbook.service.service;
 
 import com.artbook.service.domain.StubUserDetails;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StubUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    // TODO
+    @Value("${STUBBED_BCRYPT_PASSWORD}")
     private String stubbedPassword;
-
-    @PostConstruct
-    public void init() {
-        this.stubbedPassword = passwordEncoder.encode("STUBBED");
-    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
